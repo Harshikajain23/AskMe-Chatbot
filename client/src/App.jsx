f39ff09d20906fbd1ff42672a9650fb662939a78
Harshikajain23 <jainharshika2002@gmail.com>
2026-01-16 22:55:08 +0530

    Day 1 completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,12 +12,17 @@ const App = () => {
     <>
     <div className='dark:bg-gradient-to-b from-[#6B7280] to-[#1F2937] dark:text-white'>
        <div className='flex h-screen w-screen'> 
-        <Sidebar/>
-        <Routes>
-          <Route path='/' element={<ChatBox/>}/>
-           <Route path='/credits' element={<Credits/>}/>
-            <Route path='/community' element={<Community/>}/>
-        </Routes>
+        <Sidebar />
+        
+               <div className="flex-1 h-full">
+          <Routes>
+            <Route path="/" element={<ChatBox />} />
+            <Route path="/credits" element={<Credits />} />
+            <Route path="/community" element={<Community />} />
+          </Routes>
+        </div>
+        
+       
 
       </div>
     </div>
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useAppContext } from '../context/AppContext'
+import { assets } from '../assets/assets'
+import { useState } from 'react'
 
-const Sidebar = () => {
+const Sidebar = ({}) => {
+
+  const {chats, setSelectedChat, theme, setTheme, user} = useAppContext()
+  const [search, setSearch] = useState('')
+
   return (
-    <div>Sidebar</div>
+    <div className='flex flex-col h-screen w-1/6 p-5 dark:bg-gradient-to-b
+  from-[#1F1F1F]/70
+  to-[#0F0F0F]/90 border-r border-[#3F3F46]/40 backdrop-blur-3xl  transition-all duration-500 max-md:absolute left-0 z-1'>
+      {/* Logo */}
+      <img src = {theme === 'dark' ? assets.logo_full_dark : assets.logo_full} alt="" className='mt-[0] w-full max-w-80'/>
+    </div>
   )
 }
 
