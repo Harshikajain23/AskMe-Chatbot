e1294f03efb38dca23aa37931659728d86885269
Harshikajain23 <jainharshika2002@gmail.com>
2026-01-18 16:15:29 +0530

    Loading completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import ChatBox from './components/ChatBox'
 
 import { Community } from './pages/Community'
 import Credits from './pages/Credits'
 import { assets } from './assets/assets'
 import { useState } from 'react'
+import './assets/prism.css'
+import { Loading } from './pages/Loading'
 
 
 const App = () => {
 
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const {pathname} = useLocation()
+
+  if(pathname === '/loading') return <Loading />
   return (
     <>
     {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert' onClick={()=> setIsMenuOpen(true)}/>}
