57888547753433a60f3e8b682794573cb84d89e4
Harshikajain23 <jainharshika2002@gmail.com>
2026-01-18 15:58:40 +0530

    Chatbox completed

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -12,6 +12,17 @@ const ChatBox = () => {
 
   const [loading, setLoading] = useState(false)
 
+  const [prompt, setPrompt] = useState('')
+
+  const [mode, setMode] = useState('text')
+
+  const [isPublished, setIsPublished] = useState(false)
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+  }
+
   useEffect(()=>{
     if(selectedChat){
       setMessages(selectedChat.messages)
@@ -22,7 +33,7 @@ const ChatBox = () => {
   return (
     <div className='flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40'> 
     {/* Chat Messages */}
-    <div className='flex-1 mb-5 mt-30 overflow-y-scroll flex items-center'>
+    <div className='flex-1 mb-10 mt-20 overflow-y-scroll flex items-center'>
       {messages.length === 0 && (
         <div className='h-full flex flex-col items-center justify-center gap-2 text-primary m-auto'> 
           <img src={theme === 'dark' ? assets.logo_full_dark : assets.logo_full} alt="" className='w-full max-w-56 sm:max-w-68' />
@@ -31,10 +42,40 @@ const ChatBox = () => {
       )}
 
       {messages.map((message, index)=> <Message key={index} message={message}/>)}
+
+      {/* Three Dots Loading */}
+      {
+        loading && <div className='loader flex items-center gap-1.5'>
+          <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'>
+          </div>
+
+          <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'>
+          </div>
+
+          <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'>
+          </div>
+
+          </div>
+      }
     </div>
 
+    {mode === 'image' && (
+      <label className='inline-flex items-center gap-2 mb-3 text-sm mx-auto' >
+        <p className='text-xs'> Publish Generated Image to Community</p>
+        <input type='checkbox' className='cursor-pointer' checked={isPublished} onChange={(e)=> setIsPublished(e.target.checked)} />
+      </label>
+    )}
+
     {/* Prompt Input Box */}
-    <form>
+    <form onSubmit={onSubmit} className='bg-primary/20 dark:bg-[#464444] border border-primary dark:border-[#827e87]/30 rounded-full max-w-2xl p-3 pl-4 mx-auto flex gap-4 items-center'>
+      <select onChange={(e)=> setMode(e.target.value)} value={mode} className='text-sm pl-3 pr-2 outline-none'>
+        <option className='dark:bg-gray-900' value="text">Text</option> 
+        <option className='dark:bg-gray-900' value="image">Image</option>
+      </select>
+      <input onChange={(e)=> setPrompt(e.target.value)} value={prompt} type="text" placeholder='Type your prompt here...' className='flex-1 w-full text-sm outline-none' required/>
+      <button disabled = {loading}>
+        <img src={loading? assets.stop_icon : assets.send_icon} className='w-8 cursor-pointer' alt="" />
+      </button>
        </form>
     </div>
   )
